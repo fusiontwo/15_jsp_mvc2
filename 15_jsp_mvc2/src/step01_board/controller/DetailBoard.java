0ea1abb72a07a58b39850e360df48dc8fa624187
19_web_cyr <19_web_cyr@DESKTOP-R0OB2SQ>
2023-11-21 21:47:07 +0900

    .

---
+++ 15_jsp_mvc2/src/step01_board/controller/DetailBoard.java
@@ -0,0 +1,27 @@
+package step01_board.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import step01_board.dao.BoardDAO;
+
+@WebServlet("/bDetail")
+public class DetailBoard extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setAttribute("boardDTO", BoardDAO.getInstance().getBoardDetail(Long.parseLong(request.getParameter("boardId"))));
+		
+		RequestDispatcher dis = request.getRequestDispatcher("step01_boardEx/bDetail.jsp");
+		dis.forward(request, response);
+	
+	}
+
+}
---
+++ 15_jsp_mvc2/src/step01_board/dao/BoardDAO.java
@@ -0,0 +1,167 @@
+package step01_board.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import step01_board.dto.BoardDTO;
+
+public class BoardDAO {
+
+	private BoardDAO() {}
+	private static BoardDAO instance = new BoardDAO();
+	public static BoardDAO getInstance() {
+		return instance;
+	}
+	
+	private Connection conn         = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs            = null;
+	
+	private void getConnection() {
+		
+		try {
+			
+			/*
+			
+				이클립스에서 Servers폴더에 있는 Context.xml파일에 아래의 설정 추가 
+			
+				<Resource 
+					auth="Container" 
+					driverClassName="com.mysql.cj.jdbc.Driver"
+					type="javax.sql.DataSource"
+					url="jdbc:mysql://localhost:3306/MVC2_PRACTICE?serverTimezone=Asia/Seoul&amp;useSSL=false"
+					name="jdbc/board" 
+					username="root"
+					password="1234" 
+					loginTimeout="10" 
+					maxWait="5000" 
+				/> 
+			
+			 */
+			
+			/*
+			 *  import javax.naming.Context;
+			 *	import javax.naming.InitialContext;
+			 *	import javax.sql.DataSource;
+			 */
+		
+			Context initctx = new InitialContext();
+			Context envctx = (Context) initctx.lookup("java:comp/env"); // lookup 메서드를 통해 context.xml 파일에 접근하여 자바환경 코드를 검색
+			DataSource ds = (DataSource) envctx.lookup("jdbc/board"); 	// <Context>태그안의 <Resource> 환경설정의 name이 jdbc/board인 것을 검색
+			conn = ds.getConnection();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void getClose() {
+		if (rs != null) try {rs.close();} catch (SQLException e) {e.printStackTrace();}
+		if (pstmt != null) try {pstmt.close();} catch (SQLException e) {e.printStackTrace();}
+		if (conn != null) try {conn.close();} catch (SQLException e) {e.printStackTrace();}
+	}
+	
+	
+	public void insertBoard(BoardDTO boardDTO) {
+		
+		try {
+
+			getConnection();
+			
+			String sql = "INSERT INTO BOARD(WRITER,EMAIL,SUBJECT,PASSWORD,CONTENT,READ_CNT,ENROLL_DT) VALUES (?,?,?,?,?,0,NOW())";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, boardDTO.getWriter());
+			pstmt.setString(2, boardDTO.getEmail());
+			pstmt.setString(3, boardDTO.getSubject());
+			pstmt.setString(4, boardDTO.getPassword());
+			pstmt.setString(5, boardDTO.getContent());
+			
+			pstmt.executeUpdate();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			getClose();
+		}
+	}
+	
+	public ArrayList<BoardDTO> getBoardList() {
+		
+		ArrayList<BoardDTO> boardList = new ArrayList<BoardDTO>();
+		
+		try {
+
+			getConnection();
+			
+			pstmt = conn.prepareStatement("SELECT * FROM BOARD");
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				
+				BoardDTO temp = new BoardDTO();
+				temp.setBoardId(rs.getLong("BOARD_ID"));
+				temp.setWriter(rs.getString("WRITER"));
+				temp.setSubject(rs.getString("SUBJECT"));
+				temp.setReadCnt(rs.getLong("READ_CNT"));
+				temp.setEnrollDt(rs.getDate("ENROLL_DT"));
+				boardList.add(temp);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			getClose();
+		}
+		
+		System.out.println(boardList);
+		
+		return boardList;
+	}
+	
+	public BoardDTO getBoardDetail(long boardId) {
+		BoardDTO boardDTO = new BoardDTO();
+		
+		try {
+			getConnection();
+			
+			pstmt = conn.prepareStatement("UPDATE BOARD SET READ_CNT = READ_CNT + 1 WHERE BOARD_ID = ?");
+			pstmt.setLong(1, boardId);
+			pstmt.executeUpdate();
+			
+			pstmt = conn.prepareStatement("SELECT * FROM BOARD WHERE BOARD_ID = ?");
+			pstmt.setLong(1, boardId);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				
+				boardDTO.setBoardId(boardId);
+				boardDTO.setWriter(rs.getString("WRITER"));
+				boardDTO.setEmail(rs.getString("EMAIL"));
+				boardDTO.setSubject(rs.getString("SUBJECT"));
+				boardDTO.setContent(rs.getString("CONTENT"));
+				boardDTO.setReadCnt(rs.getLong("READ_CNT"));
+				boardDTO.setEnrollDt(rs.getDate("ENROLL_DT"));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			getClose();
+		}
+		
+		System.out.println(boardDTO);
+		
+		return boardDTO;
+	}
+	
+}
